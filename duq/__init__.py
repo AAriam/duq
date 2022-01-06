c0cee5754bbf36803c66ce9f0441ac10ae8cbe31
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-06 01:45:53 +0100

    updated init file to import main functionalities

---
+++ duq/__init__.py
@@ -0,0 +1,8 @@
+from .dimension import Dimension
+from .dimension import predefined as predefined_dimensions
+from .unit import Unit
+from .unit import predefined as predefined_units
+from .quantity import Quantity
+from .quantity import phys_consts as predefined_constants
+
+
