92f8073342ed704f73a5f28d38680df7ec83eb55
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-07 17:42:00 +0100

    added docstrings and type annotations

--- duq/helpers.py
+++ duq/helpers.py
@@ -1,10 +1,21 @@
-from fractions import Fraction
-import numpy as np
-from .data.unicode_chars import superscript_chars
+"""
+Helper functions used in other modules.
+"""
+
+# Standard library
 from typing import Tuple, Union, Sequence
+from fractions import Fraction
+
+# 3rd-party
+import numpy as np
+
+# Self
+from .data.unicode_chars import superscript_chars
 
 
-def parse_base_with_exp_string(string: str) -> Tuple[np.ndarray, np.ndarray]:
+def parse_base_with_exp_string(
+        string: str
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Parse a string representation of a collection of bases and exponents,
     seperated from each other by a '.' symbol. Each base may be followed
@@ -47,7 +58,44 @@ def parse_base_with_exp_string(string: str) -> Tuple[np.ndarray, np.ndarray]:
     return bases, exps
 
 
-def pretty_print_base_with_exp_series(base_array: Sequence, exp_array: Sequence, seperator: str = "") -> str:
+def generate_symbol_for_base_exp_series(
+        base_array: np.ndarray,
+        exp_array: np.ndarray,
+        seperator: str = ""
+) -> str:
+    """
+    Take two arrays representing the bases and exponents of an expression,
+    and create a string representation of the expression where the bases with
+    an exponent of 0 are ignored, bases with an exponent of 1 are written without the exponent,
+    and all other exponents are superscripted, and each base-exponent pair is separated from the
+    others by a given `separator`.
+
+    Parameters
+    ----------
+    base_array : numpy.ndarray
+        Array representing the bases.
+    exp_array : numpy.ndarray
+        Array representing the exponents
+    seperator : str
+        Separator to be printed in between base-exponent pairs.
+
+    Returns
+    -------
+        str
+        String representation of the expression.
+    """
+    nonzero_exp_mask = exp_array != 0
+    nonzero_exps = exp_array[nonzero_exp_mask]
+    bases_of_nonzero_exps = base_array[nonzero_exp_mask]
+    pretty_string_representation = pretty_print_base_with_exp_series(bases_of_nonzero_exps, nonzero_exps, seperator)
+    return pretty_string_representation
+
+
+def pretty_print_base_with_exp_series(
+        base_array: Sequence,
+        exp_array: Sequence,
+        seperator: str = ""
+) -> str:
     """
     Create a string representation of a collection of bases and their corresponding exponents,
     where exponents are in superscript and the terms may be separated by a seperator character.
@@ -90,15 +138,9 @@ def pretty_print_base_with_exp_series(base_array: Sequence, exp_array: Sequence,
         return "".join(full_expression_array)
 
 
-def generate_symbol_for_base_exp_series(base_array, exp_array, seperator=""):
-    nonzero_exp_mask = exp_array != 0
-    nonzero_exps = exp_array[nonzero_exp_mask]
-    bases_of_nonzero_exps = base_array[nonzero_exp_mask]
-    pretty_string_representation = pretty_print_base_with_exp_series(bases_of_nonzero_exps, nonzero_exps, seperator)
-    return pretty_string_representation
-
-
-def superscript_map_func(exp: Union[int, float]) -> str:
+def superscript_map_func(
+        exp: Union[int, float]
+) -> str:
     """
     Turn a number into its superscript string form.
     """
@@ -111,7 +153,31 @@ def superscript_map_func(exp: Union[int, float]) -> str:
     return "".join([superscript_chars[char] for char in exp_reduced_str])
 
 
-def raise_for_type(obj, obj_type, msg, error_type=NotImplementedError):
+def raise_for_type(
+        obj: object,
+        obj_type: type,
+        msg: str,
+        error_type: type = NotImplementedError
+) -> None:
+    """
+    Check an object's type and raise a specific error with a given message
+    if the type does not match with the given expected type.
+
+    Parameters
+    ----------
+    obj : Object
+        The object whose type is to be checked.
+    obj_type : Type
+        The expected type of the object.
+    msg : str
+        Error message to be shown when the type does not match.
+    error_type : Exception (optional; default: NotImplementedError)
+        Type of error to be raised.
+
+    Returns
+    -------
+        None
+    """
     if not isinstance(obj, obj_type):
         raise error_type(msg)
     return
