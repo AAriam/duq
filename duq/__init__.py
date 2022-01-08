ddb162453f4e85c5084aadd312238de780dac272
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-08 10:52:59 +0100

    Update __init__.py

--- duq/__init__.py
+++ duq/__init__.py
@@ -3,6 +3,6 @@ from .dimension import predefined as predefined_dimensions
 from .unit import Unit
 from .unit import predefined as predefined_units
 from .quantity import Quantity
-from .quantity import phys_consts as predefined_constants
+from .quantity import predefined as predefined_constants
 
 
