9f1d8d3947b630744e8e16dc1440376827525602
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-12 11:52:49 +0100

    added more constants

--- duq/data/physical_constants.py
+++ duq/data/physical_constants.py
@@ -15,11 +15,51 @@ phys_consts = {
             "unit": "N.m^2.C^-2"
         },
 
+    "rydberg":
+        {
+            "name": "Rydberg constant",
+            "symbol": "R_∞",
+            "value": 10973731.568160,
+            "unit": "m^-1"
+        },
+
     "boltzmann":
         {
             "name": "Boltzmann constant",
             "symbol": "k_B",
             "value": 1.380649e-32,
             "unit": "J.K^-1"
-        }
+        },
+
+    "speed_of_light":
+        {
+            "name": "speed of light",
+            "symbol": "c",
+            "value": 299792458,
+            "unit": "m.s^-1"
+        },
+
+    "planck":
+        {
+            "name": "Planck constant",
+            "symbol": "h",
+            "value": 6.62607015e-34,
+            "unit": "J.s"
+        },
+
+    "elementary_charge":
+        {
+            "name": "elementary charge",
+            "symbol": "e",
+            "value": 1.602176634e-19,
+            "unit": "C"
+        },
+
+    "gravitational_constant":
+        {
+            "name": "gravitational constant",
+            "symbol": "G",
+            "value": 6.67430e-11,
+            "unit": "m^3.kg^-1.s^-2"
+        },
 }
